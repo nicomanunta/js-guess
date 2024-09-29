062d7463c2c1885e942ad585c8212841f6330199
nicomanunta <nic.manun@gmail.com>
2024-09-29 15:09:05 +0200

    fixed enter click

--- js/script.js
+++ js/script.js
@@ -8,43 +8,47 @@ let messageResult = document.getElementById('result');
 let randomNumber;
 
 easyButton.addEventListener('click', () => {
-    let randomNumber = Math.floor(Math.random () * 10) + 1;
+    randomNumber = Math.floor(Math.random () * 10) + 1;
     randomNumberDisplay.innerText = randomNumber;
     difficultyButton.style.display = 'none';
     inputNumber.style.display = 'inline-block';
 });
 mediumButton.addEventListener('click', () => {
-    let randomNumber = Math.floor(Math.random () * 100) + 1;
+    randomNumber = Math.floor(Math.random () * 100) + 1;
     randomNumberDisplay.innerText = randomNumber;
     difficultyButton.style.display = 'none';
     inputNumber.style.display = 'inline-block';
 });
 hardButton.addEventListener('click', () => {
-    let randomNumber = Math.floor(Math.random () * 1000) + 1;
+    randomNumber = Math.floor(Math.random () * 1000) + 1;
     randomNumberDisplay.innerText = randomNumber; 
     difficultyButton.style.display = 'none';
     inputNumber.style.display = 'inline-block';
 });
 
-inputNumber.addEventListener('keypress', function(event){
+inputNumber.addEventListener('keydown', function(event){
     if (event.key === 'Enter') {
 
         let userNumber = parseInt(inputNumber.value);
 
         if (userNumber === randomNumber) {
-            messageResult.innerText = 'Hai indovinato il numero!'
+            messageResult.innerText = 'Hai indovinato il numero!';
         } else if (userNumber > randomNumber){
-            messageResult.innerText = "Ci sei quasi, un po' più piccolo!"
+            if (userNumber > randomNumber + 100) {
+                messageResult.innerText = "Sei lontano, più piccolo!";
+            } else if (userNumber > randomNumber + 20) {
+                messageResult.innerText = "Più piccolo!";
+            } else {
+                messageResult.innerText = "Ci sei quasi, un po' più piccolo!";
+            }
         } else if (userNumber < randomNumber){
-            messageResult.innerText = "Ci sei quasi, un po' più grande!"
-        } else if (userNumber > randomNumber + 10){
-            messageResult.innerText = "Più piccolo!"
-        } else if (userNumber < randomNumber - 10){
-            messageResult.innerText = "Più grande!"
-        } else if (userNumber > randomNumber + 100){
-            messageResult.innerText = "Sei lontano, più piccolo!"
-        } else if (userNumber < randomNumber - 100){
-            messageResult.innerText = "Sei lontano, più grande!"
+            if (userNumber < randomNumber - 100) {
+                messageResult.innerText = "Sei lontano, più grande!";
+            } else if (userNumber < randomNumber - 20) {
+                messageResult.innerText = "Più grande!";
+            } else {
+                messageResult.innerText = "Ci sei quasi, un po' più grande!";
+            }
         }
     }
 })
